e5d566f22a7153a4d75dfe4f39f5e91e47c180b1
Artium Nihamkin <artium@nihamkin.com>
2019-04-22 01:02:18 +0300

    Trying to use polling method

--- server.js
+++ server.js
@@ -3,11 +3,12 @@
 const Telegraf = require('telegraf')
 
 //const bot = new Telegraf(process.env.BOT_TOKEN)
-const bot = new Telegraf("880639911:AAF9e-hBEsTwdHhRrtRj93rshpix1bZ9WE0")
+const bot = new Telegraf("bot880639911:AAF9e-hBEsTwdHhRrtRj93rshpix1bZ9WE0")
 console.log("Created Bot")
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
+bot.startPolling([timeout], [limit], [allowedUpdates], [stopCallback])
 
