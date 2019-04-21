e7163385d1fb4814d94ccb036858eadae4c74c88
Artium Nihamkin <artium@nihamkin.com>
2019-04-22 01:28:37 +0300

    I think I made polling work

--- server.js
+++ server.js
@@ -3,20 +3,18 @@
 const Telegraf = require('telegraf')
 
 //const bot = new Telegraf(process.env.BOT_TOKEN)
-const bot = new Telegraf("bot880639911:AAF9e-hBEsTwdHhRrtRj93rshpix1bZ9WE0")
+const bot = new Telegraf("880639911:AAF9e-hBEsTwdHhRrtRj93rshpix1bZ9WE0")
 console.log("Created Bot")
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
 
-//bot.launch()
-
-bot.launch({
+/*bot.launch({
     webhook: {
-      domain: 'https://fortunebot.azurewebsites.net',
+      domain: 'https://127.0.0.1',
       port: 3000
   }
-})
-//bot.startPolling([timeout], [limit], [allowedUpdates], [stopCallback])
+})*/
 
