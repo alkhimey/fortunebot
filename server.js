dbb3148738dc9984a05671ebd6d2ad33018e8ba7
Artium Nihamkin <artium@nihamkin.com>
2019-04-22 15:34:36 +0300

    Add fortunes file and reading it

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+
+/**
+ * Loads the fortune strings into an in-memory db
+ * @returns an array of all fortune strings
+ */
+function loadFortuneDB() {
+    return fs.readFileSync('fortunes', 'utf8').split('%\n')
+}
+
+var fortunes = loadFortuneDB();
+
+
+
 const Telegraf = require('telegraf')
 
 //const bot = new Telegraf(process.env.BOT_TOKEN)
-const bot = new Telegraf("880639911:AAF9e-hBEsTwdHhRrtRj93rshpix1bZ9WE0")
+const bot = new Telegraf("880639911:AAF9e-hBEsTwdHhRrtRj93rshpix1bZ9WE0") // TODO: Hide token and revoke it
 console.log("Created Bot")
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', function(ctx) { 
-    ctx.reply('Hey there');
-    console.log("received hi") })
 
-//bot.launch()
+bot.hears('hi', (ctx) => ctx.reply('I have ' + fortunes.length + "fortunes to tell you."))
+
 
 bot.launch({
     webhook: {
