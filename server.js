be77cf378231fc037ecd98f5bfe75a5729e26771
Artium Nihamkin <artium@nihamkin.com>
2019-04-22 16:55:39 +0300

    Make bot pick random fortune

--- server.js
+++ server.js
@@ -8,23 +8,33 @@ var endOfLine = require('os').EOL;
  * @returns an array of all fortune strings
  */
 function loadFortuneDB() {
-    return fs.readFileSync('fortunes', 'utf8').split('%'+endOfLine).filter(x => x)
+    return fs.readFileSync('fortunes', 'utf8').split('%'+endOfLine).filter(x => x) // TODO: Handle error?
+}
+
+function getFortune() {
+    return fortunes[Math.floor(Math.random() * fortunes.length)];
 }
 
 var fortunes = loadFortuneDB();
 
-console.log(fortunes.length)
+console.log("Total fortunes in DB: " + fortunes.length)
+
 
 const Telegraf = require('telegraf')
-
 //const bot = new Telegraf(process.env.BOT_TOKEN)
 const bot = new Telegraf("880639911:AAF9e-hBEsTwdHhRrtRj93rshpix1bZ9WE0") // TODO: Hide token and revoke it
-console.log("Created Bot")
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
+
+bot.start((ctx) => ctx.reply('Welcome! Send me any message and I will reply with a random epigram.'))
+bot.help((ctx) => ctx.reply('Send me any message and I will reply with a random epigram.'))
+
+bot.on('message', (ctx) => {
+    return ctx.reply(getFortune())
+})
+
+/*
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('I have ' + fortunes.length + " fortunes to tell you."))
-
+*/
 
 bot.launch({
     webhook: {
@@ -32,4 +42,4 @@ bot.launch({
       port: process.env.PORT || 3000
   }
 })
-
+a
