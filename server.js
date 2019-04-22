06385a246fe062c4c2748572b2c1edae2362f723
Artium Nihamkin <artium@nihamkin.com>
2019-04-23 00:58:49 +0300

    Changed about text to point to this github repo

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => ctx.reply('Welcome! Send me any /fortune command and I will reply with a random epigram.'))
 bot.help((ctx) => ctx.reply('Send me a /fortune command and I will reply with a random epigram.'))
-bot.command('about', (ctx) => ctx.reply('fortune@nihamkin.com\n ' + fortunes.length + " fortunes in db"))
+bot.command('about', (ctx) => ctx.reply('https://github.com/alkhimey/fortunebot\n ' + fortunes.length + " fortunes in db"))
 bot.command('fortune', (ctx) => ctx.reply(getFortune()))
 
 bot.launch({
